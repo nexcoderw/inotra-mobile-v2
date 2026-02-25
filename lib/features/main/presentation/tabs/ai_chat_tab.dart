f9d806fc04baaba4cb43ad67431d97aff2d0eb31
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 01:01:57 +0200

    Simplify AI Chat tab now that routing guard handles auth

--- lib/features/main/presentation/tabs/ai_chat_tab.dart
+++ lib/features/main/presentation/tabs/ai_chat_tab.dart
@@ -1,38 +1,9 @@
 import "package:flutter/material.dart";
 
 import "../../../../core/config/app_routes.dart";
-import "../../../../core/services/auth_session.dart";
-import "../widgets/auth_dialog.dart";
 
-class AiChatTab extends StatefulWidget {
+class AiChatTab extends StatelessWidget {
   const AiChatTab({super.key});
-
-  @override
-  State<AiChatTab> createState() => _AiChatTabState();
-}
-
-class _AiChatTabState extends State<AiChatTab> {
-  bool _dialogShown = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) => _guardAccess());
-  }
-
-  Future<void> _guardAccess() async {
-    if (_dialogShown) return;
-    final authed = AuthSession.instance.value.isAuthenticated;
-    if (authed) return;
-    _dialogShown = true;
-    if (!mounted) return;
-    await AuthDialog.show(
-      context,
-      featureLabel: "AI Chat",
-      description:
-          "Sign in or create an account to chat with AI and keep your conversations saved.",
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
