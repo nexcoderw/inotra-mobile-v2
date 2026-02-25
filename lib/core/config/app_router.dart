5dd364aa07ab960caed9b772aff872f03fe4d9a0
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-25 15:23:11 +0200

    Add app router to generate routes for auth and main pages

---
+++ lib/core/config/app_router.dart
@@ -0,0 +1,108 @@
+import "package:flutter/material.dart";
+
+import "app_routes.dart";
+
+// Auth pages
+import "../../features/auth/presentation/pages/login_page.dart";
+import "../../features/auth/presentation/pages/register_page.dart";
+import "../../features/auth/presentation/pages/verify_registration_otp_page.dart";
+import "../../features/auth/presentation/pages/forgot_password_page.dart";
+import "../../features/auth/presentation/pages/reset_password_page.dart";
+import "../../features/auth/presentation/pages/confirm_password_reset_page.dart";
+
+// Main pages
+import "../../features/main/presentation/pages/home_page.dart";
+import "../../features/main/presentation/pages/trip_packages_page.dart";
+import "../../features/main/presentation/pages/trip_package_details_page.dart";
+import "../../features/main/presentation/pages/listings_page.dart";
+import "../../features/main/presentation/pages/listing_details_page.dart";
+import "../../features/main/presentation/pages/events_page.dart";
+import "../../features/main/presentation/pages/event_details_page.dart";
+import "../../features/main/presentation/pages/ai_chat_page.dart";
+import "../../features/main/presentation/pages/ai_chat_conversations_page.dart";
+import "../../features/main/presentation/pages/profile_page.dart";
+import "../../features/main/presentation/pages/notifications_page.dart";
+
+final class AppRouter {
+  AppRouter._();
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // Auth
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+
+      case AppRoutes.register:
+        return MaterialPageRoute(builder: (_) => const RegisterPage());
+
+      case AppRoutes.verifyRegistrationOtp:
+        final email = settings.arguments as String?;
+        return MaterialPageRoute(
+          builder: (_) => VerifyRegistrationOtpPage(email: email),
+        );
+
+      case AppRoutes.forgotPassword:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordPage());
+
+      case AppRoutes.resetPassword:
+        final email = settings.arguments as String?;
+        return MaterialPageRoute(builder: (_) => ResetPasswordPage(email: email));
+
+      case AppRoutes.confirmPasswordReset:
+        final email = settings.arguments as String?;
+        return MaterialPageRoute(
+          builder: (_) => ConfirmPasswordResetPage(email: email),
+        );
+
+      // Main
+      case AppRoutes.home:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+
+      case AppRoutes.tripPackages:
+        return MaterialPageRoute(builder: (_) => const TripPackagesPage());
+
+      case AppRoutes.tripPackageDetails:
+        final id = settings.arguments as String?;
+        return MaterialPageRoute(
+          builder: (_) => TripPackageDetailsPage(packageId: id),
+        );
+
+      case AppRoutes.listings:
+        return MaterialPageRoute(builder: (_) => const ListingsPage());
+
+      case AppRoutes.listingDetails:
+        final id = settings.arguments as String?;
+        return MaterialPageRoute(
+          builder: (_) => ListingDetailsPage(placeId: id),
+        );
+
+      case AppRoutes.events:
+        return MaterialPageRoute(builder: (_) => const EventsPage());
+
+      case AppRoutes.eventDetails:
+        final id = settings.arguments as String?;
+        return MaterialPageRoute(
+          builder: (_) => EventDetailsPage(eventId: id),
+        );
+
+      case AppRoutes.aiChat:
+        return MaterialPageRoute(builder: (_) => const AiChatPage());
+
+      case AppRoutes.aiChatConversations:
+        return MaterialPageRoute(builder: (_) => const AiChatConversationsPage());
+
+      case AppRoutes.profile:
+        return MaterialPageRoute(builder: (_) => const ProfilePage());
+
+      case AppRoutes.notifications:
+        return MaterialPageRoute(builder: (_) => const NotificationsPage());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text("Route not found")),
+          ),
+        );
+    }
+  }
+}
