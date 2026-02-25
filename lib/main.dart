5f32fb696f3cd22b3b6745738260b3dec0ad6cf4
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 00:07:24 +0200

    Restore auth session before running app

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import "package:flutter/material.dart";
 
 import "core/config/env.dart";
 import "app.dart";
+import "core/services/auth_session.dart";
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Env.load();
+  await AuthSession.instance.restore();
   runApp(const App());
 }
