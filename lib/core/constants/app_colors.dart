8eae89f93b544d618d0febb57b5955639a5c6e47
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-25 15:50:26 +0200

    Add AppColors tokens with INOTRA primary color

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+final class AppColors {
+  AppColors._();
+
+  /// INOTRA primary: #072C18
+  static const Color primary = Color(0xFF072C18);
+
+  /// Optional neutrals (you can expand later)
+  static const Color background = Color(0xFFF7F8F7);
+  static const Color surface = Colors.white;
+}
