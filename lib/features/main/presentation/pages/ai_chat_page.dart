af0a089f121f9b1d9a8fe948a3a1e52ac1a10c1d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 01:01:59 +0200

    Remove inline auth prompt from AI Chat page; rely on router guard

--- lib/features/main/presentation/pages/ai_chat_page.dart
+++ lib/features/main/presentation/pages/ai_chat_page.dart
@@ -1,39 +1,10 @@
 import "package:flutter/material.dart";
 
 import "../../../../core/config/app_routes.dart";
-import "../../../../core/services/auth_session.dart";
-import "../widgets/auth_dialog.dart";
 import "../widgets/main_scaffold.dart";
 
-class AiChatPage extends StatefulWidget {
+class AiChatPage extends StatelessWidget {
   const AiChatPage({super.key});
-
-  @override
-  State<AiChatPage> createState() => _AiChatPageState();
-}
-
-class _AiChatPageState extends State<AiChatPage> {
-  bool _dialogShown = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) => _guardAccess());
-  }
-
-  Future<void> _guardAccess() async {
-    if (_dialogShown) return;
-    final authed = AuthSession.instance.value.isAuthenticated;
-    if (authed) return;
-    _dialogShown = true;
-    if (!mounted) return;
-    await AuthDialog.show(
-      context,
-      featureLabel: "AI Chat",
-      description:
-          "Sign in or create an account to chat with AI and keep your conversations saved.",
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
