d263b93900dfa5c012b77bcadd5d976eb44800b9
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-25 13:37:24 +0200

    Add place endpoints constants

---
+++ lib/core/constants/api/place_endpoints.dart
@@ -0,0 +1,12 @@
+final class PlaceEndpoints {
+  PlaceEndpoints._();
+
+  static const list = "api/places/";
+
+  static String detail(String placeId) => "api/place/$placeId/";
+  static String book(String placeId) => "api/place/$placeId/book/";
+  static String reviews(String placeId) => "api/place/$placeId/reviews/";
+  static String addReview(String placeId) => "api/place/$placeId/add/";
+  static String reportReview(String placeId, String reviewId) =>
+      "api/place/$placeId/review/$reviewId/report/";
+}
