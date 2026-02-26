97b7d1ee3478b5ec1bbdc02c525e7fb300f53543
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 15:30:44 +0200

    Allow hiding default app bar in MainScaffold

--- lib/features/main/presentation/widgets/main_scaffold.dart
+++ lib/features/main/presentation/widgets/main_scaffold.dart
@@ -4,18 +4,20 @@ class MainScaffold extends StatelessWidget {
   final String title;
   final List<Widget>? actions;
   final Widget child;
+  final bool showAppBar;
 
   const MainScaffold({
     super.key,
     required this.title,
     required this.child,
     this.actions,
+    this.showAppBar = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title), actions: actions),
+      appBar: showAppBar ? AppBar(title: Text(title), actions: actions) : null,
       body: SafeArea(child: child),
     );
   }
