347b67aa9b89ba6201e0a47bbb5283612ebd89a4
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 18:56:51 +0200

    Localize notifications page titles and item text

--- lib/features/main/presentation/pages/notifications_page.dart
+++ lib/features/main/presentation/pages/notifications_page.dart
@@ -1,15 +1,18 @@
 import "package:flutter/material.dart";
 import "../widgets/main_scaffold.dart";
+import "../../../../i18n/lang.dart";
+import "../../../../i18n/translations.dart";
 
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final items = List.generate(6, (i) => "Notification ${i + 1}");
+    final lang = currentLangSync();
+    final items = List.generate(6, (i) => "${t(lang, "notifications.item_prefix")} ${i + 1}");
 
     return MainScaffold(
-      title: "Notifications",
+      title: t(lang, "notifications.title"),
       child: ListView.separated(
         padding: const EdgeInsets.all(16),
         itemCount: items.length,
@@ -19,7 +22,7 @@ class NotificationsPage extends StatelessWidget {
             child: ListTile(
               leading: const Icon(Icons.notifications_outlined),
               title: Text(items[index], style: const TextStyle(fontWeight: FontWeight.w600)),
-              subtitle: const Text("Details will be implemented later"),
+              subtitle: Text(t(lang, "notifications.placeholder")),
             ),
           );
         },
