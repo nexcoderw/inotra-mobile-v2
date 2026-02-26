78f79520bb8bd75f749ddd70607cc9169bafa247
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 18:56:45 +0200

    Localize trip packages list titles and subtitles

--- lib/features/main/presentation/pages/trip_packages_page.dart
+++ lib/features/main/presentation/pages/trip_packages_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "../../../../core/config/app_routes.dart";
+import "../../../../i18n/lang.dart";
+import "../../../../i18n/translations.dart";
 import "../widgets/main_scaffold.dart";
 
 class TripPackagesPage extends StatelessWidget {
@@ -8,9 +10,10 @@ class TripPackagesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final packages = List.generate(8, (i) => "PKG-${i + 1}");
+    final lang = currentLangSync();
 
     return MainScaffold(
-      title: "Trip Packages",
+      title: t(lang, "trips.title"),
       child: ListView.separated(
         padding: const EdgeInsets.all(16),
         itemCount: packages.length,
@@ -19,8 +22,9 @@ class TripPackagesPage extends StatelessWidget {
           final id = packages[index];
           return Card(
             child: ListTile(
-              title: Text("Package $id", style: const TextStyle(fontWeight: FontWeight.w600)),
-              subtitle: const Text("Tap to view details"),
+              title: Text("${t(lang, "trips.details_title")} $id",
+                  style: const TextStyle(fontWeight: FontWeight.w600)),
+              subtitle: Text(t(lang, "common.tap_details")),
               trailing: const Icon(Icons.chevron_right),
               onTap: () => Navigator.pushNamed(
                 context,
