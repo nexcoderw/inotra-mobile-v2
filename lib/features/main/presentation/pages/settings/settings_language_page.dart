695847d6007f8322df81b81884cd3afbebfd6ba6
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 17:30:54 +0200

    Send backend language labels and import jsonEncode for language update

--- lib/features/main/presentation/pages/settings/settings_language_page.dart
+++ lib/features/main/presentation/pages/settings/settings_language_page.dart
@@ -2,7 +2,6 @@ import "dart:convert";
 
 import "package:flutter/material.dart";
 import "package:hugeicons/hugeicons.dart";
-import "package:provider/provider.dart";
 import "package:toastification/toastification.dart";
 import "package:http/http.dart" as http;
 
