57b4e9d02bbbe215f4677709eee4aa2f844b9240
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 17:22:38 +0200

    Register guarded settings language route

--- lib/core/config/app_router.dart
+++ lib/core/config/app_router.dart
@@ -21,6 +21,7 @@ import "../../features/main/presentation/pages/profile_page.dart";
 import "../../features/main/presentation/pages/notifications_page.dart";
 import "../../features/main/presentation/pages/settings/settings_page.dart";
 import "../../features/main/presentation/pages/settings/settings_theme_page.dart";
+import "../../features/main/presentation/pages/settings/settings_language_page.dart";
 import "../guards/auth_guard.dart";
 
 final class AppRouter {
@@ -133,6 +134,13 @@ final class AppRouter {
         featureLabel: "Theme",
         description: "Sign in to change your app appearance.",
         builder: (_) => const SettingsThemePage(),
+      );
+
+    case AppRoutes.settingsLanguage:
+      return AuthGuard.protect(
+        featureLabel: "Language",
+        description: "Sign in to update your preferred language.",
+        builder: (_) => const SettingsLanguagePage(),
       );
 
       // -------------------
