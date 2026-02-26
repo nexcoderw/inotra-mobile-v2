7bcbd35c9eb1ffcc2fd34c339799031d95096fa1
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 23:38:56 +0200

    Switch confirm reset page to shared InfoPill widget

--- lib/features/auth/presentation/pages/confirm_password_reset_page.dart
+++ lib/features/auth/presentation/pages/confirm_password_reset_page.dart
@@ -15,6 +15,7 @@ import "../../../../i18n/lang.dart";
 import "../../../../i18n/translations.dart";
 import "../widgets/auth_scaffold.dart";
 import "../widgets/auth_ui.dart";
+import "../widgets/info_pill.dart";
 
 class ConfirmPasswordResetPage extends StatefulWidget {
   final String? email;
@@ -256,56 +257,6 @@ class _ConfirmPasswordResetPageState extends State<ConfirmPasswordResetPage> {
                 label: tr("auth.save_changes"),
                 busy: _isBusy,
                 onTap: _isBusy ? null : _onSave,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _InfoPill extends StatelessWidget {
-  final dynamic icon;
-  final String text;
-
-  const _InfoPill({required this.icon, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(999),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-          decoration: BoxDecoration(
-            color: scheme.surface.withOpacity(0.55),
-            border: Border.all(color: scheme.onSurface.withOpacity(0.10)),
-            borderRadius: BorderRadius.circular(999),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              HugeIcon(
-                icon: icon,
-                size: 14,
-                strokeWidth: 2,
-                color: scheme.primary.withOpacity(0.95),
-              ),
-              const SizedBox(width: 8),
-              Flexible(
-                child: Text(
-                  text,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: scheme.onSurface.withOpacity(0.80),
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
               ),
             ],
           ),
