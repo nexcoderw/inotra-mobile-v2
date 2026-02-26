6c47add695aa97a7e9f8d96c5089c3a3e00548e3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 18:56:50 +0200

    Translate trip package details page title and placeholder

--- lib/features/main/presentation/pages/trip_package_details_page.dart
+++ lib/features/main/presentation/pages/trip_package_details_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "../widgets/main_scaffold.dart";
+import "../../../../i18n/lang.dart";
+import "../../../../i18n/translations.dart";
 
 class TripPackageDetailsPage extends StatelessWidget {
   final String? packageId;
@@ -7,15 +9,16 @@ class TripPackageDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final lang = currentLangSync();
     return MainScaffold(
-      title: "Package Details",
+      title: t(lang, "trips.details_title"),
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Text(
-              "Trip Package Details\n\nPackage ID: ${packageId ?? "N/A"}\n\nTODO: Fetch and display real package data.",
+              "${t(lang, "trips.details_title")}\\n\\nPackage ID: ${packageId ?? "N/A"}\\n\\n${t(lang, "common.coming_soon")}",
               style: const TextStyle(height: 1.4),
             ),
           ),
