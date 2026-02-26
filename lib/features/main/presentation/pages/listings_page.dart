191245977ab1b081f300b13eb7e65b838db0d6a0
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-26 18:53:02 +0200

    Translate listings page title and item copy

--- lib/features/main/presentation/pages/listings_page.dart
+++ lib/features/main/presentation/pages/listings_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "../../../../core/config/app_routes.dart";
+import "../../../../i18n/lang.dart";
+import "../../../../i18n/translations.dart";
 import "../widgets/main_scaffold.dart";
 
 class ListingsPage extends StatelessWidget {
@@ -8,9 +10,10 @@ class ListingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final listings = List.generate(10, (i) => "PLACE-${i + 1}");
+    final lang = currentLangSync();
 
     return MainScaffold(
-      title: "Listings",
+      title: t(lang, "nav.listings"),
       child: ListView.separated(
         padding: const EdgeInsets.all(16),
         itemCount: listings.length,
@@ -20,8 +23,9 @@ class ListingsPage extends StatelessWidget {
           return Card(
             child: ListTile(
               leading: const Icon(Icons.place_outlined),
-              title: Text("Listing $id", style: const TextStyle(fontWeight: FontWeight.w600)),
-              subtitle: const Text("Tap to view details"),
+              title: Text("${t(lang, "nav.listings")} $id",
+                  style: const TextStyle(fontWeight: FontWeight.w600)),
+              subtitle: Text(t(lang, "common.tap_details")),
               trailing: const Icon(Icons.chevron_right),
               onTap: () => Navigator.pushNamed(
                 context,
