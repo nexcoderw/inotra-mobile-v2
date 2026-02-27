f5866109e5e6eb731c2cc022178467c4b674242b
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 14:39:22 +0200

    Wrap dashboard page with MeShell layout

--- lib/features/me/presentation/pages/dashboard_page.dart
+++ lib/features/me/presentation/pages/dashboard_page.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 import "package:inotra/i18n/lang.dart";
 import "package:inotra/i18n/translations.dart";
-import "package:inotra/features/main/presentation/widgets/main_scaffold.dart";
+import "package:inotra/features/me/presentation/layouts/me_shell.dart";
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -11,7 +11,7 @@ class DashboardPage extends StatelessWidget {
     final lang = currentLangSync();
     final scheme = Theme.of(context).colorScheme;
 
-    return MainScaffold(
+    return MeShell(
       title: t(lang, "nav.dashboard"),
       child: Center(
         child: Column(
