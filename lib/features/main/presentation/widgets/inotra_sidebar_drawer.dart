9f113f4a5d73416e1df58e8e92f47f7843766377
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 13:43:23 +0200

    Route sidebar dashboard tile to DashboardPage directly

--- lib/features/main/presentation/widgets/inotra_sidebar_drawer.dart
+++ lib/features/main/presentation/widgets/inotra_sidebar_drawer.dart
@@ -116,7 +116,7 @@ class InotraSidebarDrawer extends StatelessWidget {
                         icon: HugeIcons.strokeRoundedDashboardSquare01,
                         title: t(_lang, "nav.dashboard"),
                         subtitle: t(_lang, "nav.highlights"),
-                        onTap: onDashboardTap,
+                        onTap: () => _open(context, const DashboardPage()),
                         showTrailing: false,
                       ),
 
