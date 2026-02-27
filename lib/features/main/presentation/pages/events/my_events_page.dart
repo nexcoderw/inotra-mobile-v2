9bd32a0258a12c2e6c80cec40142129c5ddd76a4
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 12:44:46 +0200

    Add my events placeholder page

---
+++ lib/features/main/presentation/pages/events/my_events_page.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+import "../../../../../i18n/lang.dart";
+import "../../../../../i18n/translations.dart";
+import "../../widgets/main_scaffold.dart";
+
+class MyEventsPage extends StatelessWidget {
+  const MyEventsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = currentLangSync();
+    final scheme = Theme.of(context).colorScheme;
+
+    return MainScaffold(
+      title: t(lang, "nav.my_events"),
+      child: Center(
+        child: Text(
+          t(lang, "common.coming_soon"),
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w800,
+            color: scheme.onSurface.withOpacity(0.8),
+          ),
+        ),
+      ),
+    );
+  }
+}
