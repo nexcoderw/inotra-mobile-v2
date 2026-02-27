6064d17911800bd8a221b381eba06bd678c4af15
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 14:39:48 +0200

    Wrap event tickets page with MeShell layout

--- lib/features/me/presentation/pages/events/event_tickets_page.dart
+++ lib/features/me/presentation/pages/events/event_tickets_page.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 import "package:inotra/i18n/lang.dart";
 import "package:inotra/i18n/translations.dart";
-import "package:inotra/features/main/presentation/widgets/main_scaffold.dart";
+import "package:inotra/features/me/presentation/layouts/me_shell.dart";
 
 class EventTicketsPage extends StatelessWidget {
   const EventTicketsPage({super.key});
@@ -11,7 +11,7 @@ class EventTicketsPage extends StatelessWidget {
     final lang = currentLangSync();
     final scheme = Theme.of(context).colorScheme;
 
-    return MainScaffold(
+    return MeShell(
       title: t(lang, "nav.event_tickets"),
       child: Center(
         child: Column(
