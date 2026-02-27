2aaac09382677eb7d7367ee775a4a81f0e507562
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 12:54:53 +0200

    Move dashboard page under me/presentation structure

---
+++ lib/features/me/presentation/pages/dashboard_page.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+import "../../../../i18n/lang.dart";
+import "../../../../i18n/translations.dart";
+import "../../../main/presentation/widgets/main_scaffold.dart";
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = currentLangSync();
+    final scheme = Theme.of(context).colorScheme;
+
+    return MainScaffold(
+      title: t(lang, "nav.dashboard"),
+      child: Center(
+        child: Text(
+          t(lang, "common.coming_soon"),
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w800,
+            color: scheme.onSurface.withOpacity(0.8),
+          ),
+        ),
+      ),
+    );
+  }
+}
