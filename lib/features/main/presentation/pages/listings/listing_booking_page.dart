7426e84d4996e7767f8eaedae048dd97fc6656cf
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-27 12:45:08 +0200

    Add listing booking placeholder page

---
+++ lib/features/main/presentation/pages/listings/listing_booking_page.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+import "../../../../../i18n/lang.dart";
+import "../../../../../i18n/translations.dart";
+import "../../widgets/main_scaffold.dart";
+
+class ListingBookingPage extends StatelessWidget {
+  const ListingBookingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = currentLangSync();
+    final scheme = Theme.of(context).colorScheme;
+
+    return MainScaffold(
+      title: t(lang, "nav.listing_booking"),
+      child: Center(
+        child: Text(
+          t(lang, "common.coming_soon"),
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w800,
+            color: scheme.onSurface.withOpacity(0.8),
+          ),
+        ),
+      ),
+    );
+  }
+}
