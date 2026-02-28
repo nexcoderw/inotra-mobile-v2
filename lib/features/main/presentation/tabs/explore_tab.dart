d6a790b93c10bd7e62aac0e4369ffb7cbf3f5eca
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-28 21:23:19 +0200

    Wire featured places and adjust explore layout

--- lib/features/main/presentation/tabs/explore_tab.dart
+++ lib/features/main/presentation/tabs/explore_tab.dart
@@ -4,6 +4,7 @@ import "../../../../i18n/translations.dart";
 import "package:inotra/features/main/presentation/widgets/trip_packages_preview.dart";
 import "package:inotra/features/main/presentation/widgets/static_ad_banner.dart";
 import "package:inotra/features/main/presentation/widgets/explore_hero_heading.dart";
+import "package:inotra/features/main/presentation/widgets/explore_places_feature.dart";
 
 class ExploreTab extends StatelessWidget {
   const ExploreTab({super.key});
@@ -24,6 +25,8 @@ class ExploreTab extends StatelessWidget {
             TripPackagesPreview(),
             const SizedBox(height: 24),
             const StaticAdBanner(),
+            const SizedBox(height: 28),
+            const ExplorePlacesFeature(),
           ],
         ),
       ),
