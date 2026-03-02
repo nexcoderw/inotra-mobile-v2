d73002b78328abe2e60bb66129c2571df9b9a5da
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-02 22:32:38 +0200

    Use shared page header on trip package details

--- lib/features/main/presentation/pages/trip_package_details_page.dart
+++ lib/features/main/presentation/pages/trip_package_details_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "../widgets/main_scaffold.dart";
+import "../widgets/page_header.dart";
 import "../../../../i18n/lang.dart";
 import "../../../../i18n/translations.dart";
 
@@ -14,28 +15,42 @@ class TripPackageDetailsPage extends StatelessWidget {
 
     return MainScaffold(
       title: t(lang, "trips.details_title"),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              t(lang, "trips.details_title"),
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w900,
-                color: scheme.onSurface.withOpacity(0.92),
+      showAppBar: false,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PageHeader(title: t(lang, "trips.details_title")),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        t(lang, "trips.details_title"),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w900,
+                          color: scheme.onSurface.withOpacity(0.92),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        t(lang, "common.coming_soon"),
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: scheme.onSurface.withOpacity(0.72),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              t(lang, "common.coming_soon"),
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-                color: scheme.onSurface.withOpacity(0.72),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
