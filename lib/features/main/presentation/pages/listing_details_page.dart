d9daa5116c63d0131ea668b2aafac0b53e9102bb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-02 16:33:45 +0200

    Fix listing reviews tab to use sheet place id

--- lib/features/main/presentation/pages/listing_details_page.dart
+++ lib/features/main/presentation/pages/listing_details_page.dart
@@ -395,7 +395,7 @@ class _DetailsSheet extends StatelessWidget {
                         children: [
                           ListingOverviewTab(place: place),
                           ListingMapTab(place: place),
-                                  ListingReviewsTab(placeId: _place!.id),
+                          ListingReviewsTab(placeId: place.id),
                           ListingTransportTab(place: place),
                         ],
                       ),
