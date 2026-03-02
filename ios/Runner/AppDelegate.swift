bd72390236621c0abe2a21ac3bc92afc8d42e798
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-02 19:28:46 +0200

    Configure Google Maps API key in iOS AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // ✅ Google Maps API Key
+    GMSServices.provideAPIKey("AIzaSyBxDQkBVb0L9RtzxNoz-XqolzbE0C_xisA")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
