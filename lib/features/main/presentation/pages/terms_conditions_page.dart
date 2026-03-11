e3823eb7d96f741a72bedcaabc0318310a8fcabf
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-11 12:33:31 +0200

    Use available icon on terms & conditions placeholder page

--- lib/features/main/presentation/pages/terms_conditions_page.dart
+++ lib/features/main/presentation/pages/terms_conditions_page.dart
@@ -38,7 +38,7 @@ class TermsConditionsPage extends StatelessWidget {
                       Row(
                         children: [
                           HugeIcon(
-                            icon: HugeIcons.strokeRoundedDocument02,
+                            icon: HugeIcons.strokeRoundedDocument01,
                             size: 20,
                             color: scheme.primary,
                           ),
