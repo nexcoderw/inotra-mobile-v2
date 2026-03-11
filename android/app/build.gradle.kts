4049eac4fdbabca7c4d4454f29a9925c04edf002
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-11 19:07:56 +0200

    Configure Android app module for Firebase Analytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.inotra"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // Required for local_auth biometrics and encryptedSharedPreferences
+        minSdk = flutter.minSdkVersion // Required for local_auth biometrics and encryptedSharedPreferences
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
