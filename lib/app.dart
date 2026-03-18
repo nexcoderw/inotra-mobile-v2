d146778e9322910ede6c5f3574650177e8be24ea
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-18 13:03:14 +0200

    chore(mobile): add firebase_messaging and flutter_local_notifications dependencies

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,12 @@
 import "package:flutter/material.dart";
-
-import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 
 import "core/config/app_router.dart";
 import "core/config/app_routes.dart";
 import "core/constants/app_colors.dart";
 import "core/observers/audit_route_observer.dart";
+import "core/services/fcm_service.dart";
+import "core/services/notification_service.dart";
 import "core/services/theme_notifier.dart";
 
 class App extends StatelessWidget {
@@ -14,11 +14,14 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeNotifier()..load(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeNotifier()..load()),
+        ChangeNotifierProvider.value(value: NotificationService.instance),
+      ],
       builder: (context, _) {
         final notifier = context.watch<ThemeNotifier>();
-        final mode = notifier.mode;
+        final mode     = notifier.mode;
 
         final lightScheme = ColorScheme.fromSeed(
           seedColor: AppColors.primary,
@@ -39,6 +42,10 @@ class App extends StatelessWidget {
           title: "INOTRA APP",
           debugShowCheckedModeBanner: false,
           themeMode: mode,
+
+          // Global navigator key used by FCMService for deep-link navigation
+          navigatorKey: FCMService.navigatorKey,
+
           theme: ThemeData(
             useMaterial3: true,
             fontFamily: "DMSans",
