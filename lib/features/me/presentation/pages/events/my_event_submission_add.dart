a4540fb7600e5868600726576820ecd302cab754
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-24 17:50:05 +0200

    Route mobile add-event page through shared event workspace

--- lib/features/me/presentation/pages/events/my_event_submission_add.dart
+++ lib/features/me/presentation/pages/events/my_event_submission_add.dart
@@ -1,95 +1,12 @@
 import "package:flutter/material.dart";
 
-import "../../../../main/presentation/widgets/page_header.dart";
-import "../../../../../i18n/lang.dart";
-import "../../../../../i18n/translations.dart";
+import "my_event_form_workspace.dart";
 
 class MyEventSubmissionAddPage extends StatelessWidget {
   const MyEventSubmissionAddPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final lang = currentLangSync();
-    final scheme = Theme.of(context).colorScheme;
-
-    return SafeArea(
-      child: ListView(
-        padding: const EdgeInsets.fromLTRB(18, 12, 18, 24),
-        children: [
-          PageHeader(title: t(lang, "my_events.add_event")),
-          _WorkspaceCard(
-            icon: Icons.add_circle_outline_rounded,
-            title: t(lang, "my_events.add_event"),
-            description: t(lang, "common.coming_soon"),
-            scheme: scheme,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _WorkspaceCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String description;
-  final ColorScheme scheme;
-
-  const _WorkspaceCard({
-    required this.icon,
-    required this.title,
-    required this.description,
-    required this.scheme,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(24),
-        color: scheme.surface,
-        border: Border.all(color: scheme.outlineVariant.withValues(alpha: 0.6)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.04),
-            blurRadius: 24,
-            offset: const Offset(0, 12),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 52,
-            height: 52,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              color: scheme.primary.withValues(alpha: 0.12),
-            ),
-            child: Icon(icon, color: scheme.primary),
-          ),
-          const SizedBox(height: 18),
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w900,
-              color: scheme.onSurface,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            description,
-            style: TextStyle(
-              fontSize: 14,
-              height: 1.5,
-              color: scheme.onSurface.withValues(alpha: 0.68),
-            ),
-          ),
-        ],
-      ),
-    );
+    return const MyEventFormWorkspace(isEditing: false);
   }
 }
