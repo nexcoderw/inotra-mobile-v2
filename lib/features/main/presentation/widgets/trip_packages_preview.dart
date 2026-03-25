645d298f6dc1d77b34010489cb7605b686d49577
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-25 16:26:57 +0200

    Use cached repository data in mobile trip packages preview

--- lib/features/main/presentation/widgets/trip_packages_preview.dart
+++ lib/features/main/presentation/widgets/trip_packages_preview.dart
@@ -1,12 +1,9 @@
-import "dart:convert";
 import "dart:ui";
 
 import "package:flutter/material.dart";
-import "package:http/http.dart" as http;
 
-import "../../../../core/config/api.dart";
 import "../../../../core/config/app_routes.dart";
-import "../../../../core/constants/api/package_endpoints.dart";
+import "../../../../core/repositories/public_discovery_repository.dart";
 import "../../../../core/widgets/app_cached_image.dart";
 import "../../../../i18n/lang.dart";
 import "../../../../i18n/translations.dart";
@@ -48,18 +45,18 @@ class _TripPackagesPreviewState extends State<TripPackagesPreview>
     super.dispose();
   }
 
-  Future<void> _load() async {
+  Future<void> _load({bool forceRefresh = false}) async {
     setState(() {
       _loading = true;
       _error = null;
     });
 
     try {
-      final uri = Api.url("${PackageEndpoints.list}?limit=3");
-      final resp = await http.get(uri);
+      final resp = await PublicDiscoveryRepository.instance
+          .fetchTripPackagesPreview(limit: 3, forceRefresh: forceRefresh);
 
-      if (resp.statusCode >= 200 && resp.statusCode < 300) {
-        final decoded = jsonDecode(resp.body);
+      if (resp.isSuccess) {
+        final decoded = resp.decodeJson();
         final results =
             (decoded is Map ? decoded["results"] : decoded) as List? ?? [];
         _items = results
@@ -121,7 +118,7 @@ class _TripPackagesPreviewState extends State<TripPackagesPreview>
               : _error != null
               ? _ErrorState(
                   message: _error!,
-                  onRetry: _load,
+                  onRetry: () => _load(forceRefresh: true),
                   tryAgainText: t(lang, "common.try_again"),
                 )
               : _items.isEmpty
